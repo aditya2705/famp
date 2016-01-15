fe207d0c2f10a5a9103a1e8585722a457db0b082
rushabhmshah99 <rushabhmshah99@gmail.com>
2016-01-15 20:59:45 +0530

    Swipeable Deck - Tinder like

--- app/src/main/java/com/alphalabz/familyapp/Adapters/BaseAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.alphalabz.familyapp.Adapters;
-
-/**
- * Created by Aditya Rathi on 16-Dec-15.
- */
-public class BaseAdapter {
-}
--- app/src/main/java/com/alphalabz/familyapp/Fragments/GalleryFragment.java
+++ app/src/main/java/com/alphalabz/familyapp/Fragments/GalleryFragment.java
@@ -3,30 +3,26 @@ package com.alphalabz.familyapp.Fragments;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 
-import com.afollestad.materialdialogs.MaterialDialog;
+import com.alphalabz.familyapp.Adapters.SwipeDeckAdapter;
 import com.alphalabz.familyapp.R;
-import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.daprlabs.cardstack.SwipeDeck;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.dexafree.materialList.card.Card;
 import com.dexafree.materialList.card.CardProvider;
-import com.dexafree.materialList.listeners.RecyclerItemClickListener;
-import com.dexafree.materialList.view.MaterialListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
     private Dialog d;
     private SubsamplingScaleImageView imageView;
+
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -42,7 +38,7 @@ public class GalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_gallery, container, false);
-        MaterialListView mListView = (MaterialListView) v.findViewById(R.id.material_listview);
+       /* MaterialListView mListView = (MaterialListView) v.findViewById(R.id.material_listview);
         mListView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(@NonNull Card card, int position) {
@@ -61,6 +57,48 @@ public class GalleryFragment extends Fragment {
         }
 
         mListView.getAdapter().addAll(cards);
+        */
+        final SwipeDeck cardStack = (SwipeDeck) v.findViewById(R.id.swipe_deck);
+
+        final ArrayList<Integer> testData = new ArrayList<>();
+        testData.add(R.drawable.gallery_1);
+        testData.add(R.drawable.gallery_2);
+        testData.add(R.drawable.gallery_3);
+        testData.add(R.drawable.background);
+        final ArrayList<Integer> testData2 = new ArrayList<>();
+        testData2.add(R.drawable.gallery_1);
+        testData2.add(R.drawable.gallery_2);
+        testData2.add(R.drawable.gallery_3);
+        testData2.add(R.drawable.background);
+
+        final SwipeDeckAdapter adapter = new SwipeDeckAdapter(testData, getContext());
+        cardStack.setAdapter(adapter);
+
+        cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
+            @Override
+            public void cardSwipedLeft(int position) {
+                Log.i("MainActivity", "card was swiped left, position in adapter: " + position);
+                testData.add(testData2.get(position % testData2.size()));
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+
+            @Override
+            public void cardSwipedRight(int position) {
+                Log.i("MainActivity", "card was swiped right, position in adapter: " + position);
+                testData.add(testData2.get(position % testData2.size()));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void cardsDepleted() {
+                Log.i("MainActivity", "no more cards");
+
+            }
+        });
+
         return v;
     }
 
@@ -77,42 +115,6 @@ public class GalleryFragment extends Fragment {
         return card;
     }
 
-    public void showDialog(String title, String content) {
-        boolean wrapInScrollView = true;
 
-        MaterialDialog md = new MaterialDialog.Builder(getContext())
-                .title(title)
-                .customView(R.layout.image_zoom_view, wrapInScrollView)
-                .positiveText("Back")
-                .backgroundColorRes(R.color.md_white_1000)
-                .positiveColorRes(R.color.md_green_500)
-                .build();
-        View par = md.getView();
-        android.view.ViewGroup.LayoutParams params = par.getLayoutParams();
-        params.width = WindowManager.LayoutParams.MATCH_PARENT;
-        params.height = WindowManager.LayoutParams.MATCH_PARENT;
-        par.setLayoutParams(params);
-        View v = md.getCustomView();
-        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView) v.findViewById(R.id.imageView);
-        imageView.setImage(ImageSource.resource(R.drawable.background_profile));
-        md.show();
-
-
-    }
-
-    public void showCustomDialog() {
-
-
-        d = new Dialog(getActivity());
-
-        d.setContentView(R.layout.image_zoom_view);
-        d.setTitle("Image");
-        imageView = (SubsamplingScaleImageView) d.findViewById(R.id.imageView);
-        imageView.setImage(ImageSource.resource(R.drawable.background));
-
-
-        d.show();
-
-    }
 
 }

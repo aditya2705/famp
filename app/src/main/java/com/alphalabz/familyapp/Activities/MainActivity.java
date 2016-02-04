51aeae9509df26028fa6e1a7324a43a31c4da80e
Aditya Rathi <adityarathi95@gmail.com>
2016-02-04 10:12:30 +0530

    Refresh option

--- app/src/main/java/com/alphalabz/familyapp/Activities/MainActivity.java
+++ app/src/main/java/com/alphalabz/familyapp/Activities/MainActivity.java
@@ -1,12 +1,20 @@
 package com.alphalabz.familyapp.Activities;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -27,6 +35,12 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private static Context mContext;
@@ -37,51 +51,40 @@ public class MainActivity extends AppCompatActivity {
         return mContext;
     }
 
+    private String membersListJsonString;
+    private static final String RESULTS_FETCH_URL = "http://alpha95.net63.net/get_members.php";
+    private SharedPreferences sharedPreferences;
+
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle b = getIntent().getExtras();
-        if (b != null && b.getInt("val") == 1) {
-            String value = b.getString("key");
-            showDialog("Event", value, "Okay");
-        }
-
-
-
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Loading...");
+        progressDialog.setCancelable(false);
+
+        sharedPreferences = getSharedPreferences("FAMP",0);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         mContext = getApplicationContext();
-
-        //Account header for the Google Material Drawer
-        //Responsive to multiple accounts
-        AccountHeader headerResult = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withCompactStyle(true)
-                .withHeaderBackground(R.drawable.header)
-                .withTextColorRes(R.color.md_white_1000)
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
-                        return false;
-                    }
-                })
-                .build();
-
 
         //Create the drawer
         result = new DrawerBuilder(this)
                 //this layout have to contain child layouts
                 .withRootView(R.id.drawer_container)
-                .withAccountHeader(headerResult)
                 .withToolbar(toolbar)
                 .withActionBarDrawerToggleAnimated(true)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName("Home").withIcon(FontAwesome.Icon.faw_android),
-                        new PrimaryDrawerItem().withName("Profile").withIcon(FontAwesome.Icon.faw_android),
-                        new PrimaryDrawerItem().withName("Events").withIcon(FontAwesome.Icon.faw_android),
-                        new PrimaryDrawerItem().withName("Data Table").withIcon(FontAwesome.Icon.faw_android),
-                        new PrimaryDrawerItem().withName("Gallery").withIcon(FontAwesome.Icon.faw_android)
+                        new PrimaryDrawerItem().withName("Home").withIcon(FontAwesome.Icon.faw_home),
+                        new PrimaryDrawerItem().withName("Profile").withIcon(FontAwesome.Icon.faw_user),
+                        new PrimaryDrawerItem().withName("Events").withIcon(FontAwesome.Icon.faw_birthday_cake),
+                        new PrimaryDrawerItem().withName("Data Table").withIcon(FontAwesome.Icon.faw_table),
+                        new PrimaryDrawerItem().withName("Gallery").withIcon(FontAwesome.Icon.faw_image)
 
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
@@ -93,11 +96,11 @@ public class MainActivity extends AppCompatActivity {
                             getSupportActionBar().setTitle(name);
                             Fragment fragment;
                             switch (i) {
-                                case 1:
+                                case 0:
                                     fragment = new TreeViewFragment();
                                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                                     break;
-                                case 2:
+                                case 1:
                                     Bundle b = new Bundle();
                                     b.putString("profile_id", "rms123");
 
@@ -105,15 +108,15 @@ public class MainActivity extends AppCompatActivity {
                                     fragment.setArguments(b);
                                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                                     break;
-                                case 3:
+                                case 2:
                                     fragment = new EventsFragment();
                                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                                     break;
-                                case 4:
+                                case 3:
                                     fragment = new EventTableFragment();
                                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                                     break;
-                                case 5:
+                                case 4:
                                     fragment = new GalleryFragment();
                                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                                     break;
@@ -160,6 +163,88 @@ public class MainActivity extends AppCompatActivity {
                 .positiveColorRes(R.color.md_green_500)
                 .build();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.refresh) {
+            getData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getData(){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                URL obj = null;
+                String result = null;
+                InputStream inputStream = null;
+                try {
+                    obj = new URL(RESULTS_FETCH_URL);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                    //add request header
+                    con.setRequestProperty("Content-Type","application/json");
+                    inputStream = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(inputStream, "UTF-8"), 8);
+
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                    Log.d("RESULT",result);
+
+                } catch (Exception e) {}
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                progressDialog.dismiss();
+                membersListJsonString =result;
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("MEMBERS_STRING",membersListJsonString);
+                editor.apply();
+
+                if(getSupportFragmentManager().findFragmentById(R.id.fragment_container) instanceof TreeViewFragment){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TreeViewFragment()).commit();
+                }
+            }
+
+            @Override
+            protected void onPreExecute() {
+                progressDialog.show();
+                super.onPreExecute();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute();
     }
 
 
